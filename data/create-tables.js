fb6c2e44fc000e8b3ef57f2862f3a85434d0196d
Kubi Kubisiak <mckubisiak@gmail.com>
2021-06-29 13:58:11 -0700

    npm install ran, data and tables updated to todo

--- data/create-tables.js
+++ data/create-tables.js
@@ -19,8 +19,8 @@ async function run() {
                 );           
                 CREATE TABLE animals (
                     id SERIAL PRIMARY KEY NOT NULL,
-                    name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
+                    todo VARCHAR(512) NOT NULL,
+                    complete BOOLEAN NOT NULL,
                     owner_id INTEGER NOT NULL REFERENCES users(id)
             );
         `);
--- data/load-seed-data.js
+++ data/load-seed-data.js
@@ -1,6 +1,6 @@
 const client = require('../lib/client');
 // import our seed data:
-const animals = require('./animals.js');
+const todoList = require('./todo-list.js');
 const usersData = require('./users.js');
 const { getEmoji } = require('../lib/emoji.js');
 
@@ -25,12 +25,12 @@ async function run() {
     const user = users[0].rows[0];
 
     await Promise.all(
-      animals.map(animal => {
+      todoList.map(todo => {
         return client.query(`
-                    INSERT INTO animals (name, cool_factor, owner_id)
+                    INSERT INTO todo-list (todo, complete, owner_id)
                     VALUES ($1, $2, $3);
                 `,
-        [animal.name, animal.cool_factor, user.id]);
+        [todo.todo, todo.complete, user.id]);
       })
     );
     

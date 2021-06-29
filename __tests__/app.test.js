41069766851dc92a927c4d9a021539c472268d3b
Kubi Kubisiak <mckubisiak@gmail.com>
2021-06-29 16:27:47 -0700

    completed test and passing for post

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,31 +28,35 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    test('returns animals', async() => {
+    test('create todo', async() => {
 
       const expectation = [
         {
-          'id': 1,
-          'name': 'bessie',
-          'cool_factor': 3,
-          'owner_id': 1
+          'id': 4,
+          'todo': 'eat snakes on a plaen',
+          'complete': false,
+          'owner_id': 2
         },
         {
-          'id': 2,
-          'name': 'jumpy',
-          'cool_factor': 4,
-          'owner_id': 1
-        },
-        {
-          'id': 3,
-          'name': 'spot',
-          'cool_factor': 10,
-          'owner_id': 1
+          'id': 5,
+          'todo': 'routes run with authorization',
+          'complete': false,
+          'owner_id': 2
         }
       ];
 
+      for (let todo of expectation) {
+        await fakeRequest(app)
+          .post('/api/todolist')
+          .send(todo)
+          .set('Authorization', token)
+          .expect('Content-Type', /json/)
+          .expect(200);
+      }
+
       const data = await fakeRequest(app)
-        .get('/animals')
+        .get('/api/todolist')
+        .set('Authorization', token)
         .expect('Content-Type', /json/)
         .expect(200);
 
